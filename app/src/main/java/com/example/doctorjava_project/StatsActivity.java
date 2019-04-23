e2a8883487dd6d6ab899635225dbfa2e42a4f689
TopiasPeiponen <topiaspeiponen@metropolia.fi>
2019-04-23 13:31:19 +0300

    Created basic layouts for all the wanted screens (Home, store, stats and settings)

--- app/src/main/java/com/example/doctorjava_project/StatsActivity.java
+++ app/src/main/java/com/example/doctorjava_project/StatsActivity.java
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class StatsActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_stats);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
