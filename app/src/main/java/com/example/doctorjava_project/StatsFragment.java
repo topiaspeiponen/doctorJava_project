10d697ce8694dc65f6f1c81810d171973b8a9f6c
Topias <topias.peiponen@metropolia.fi>
2019-05-01 18:14:52 +0300

    Added a basic functioning database. Only includes a test version on the Stats page.

--- app/src/main/java/com/example/doctorjava_project/StatsFragment.java
+++ app/src/main/java/com/example/doctorjava_project/StatsFragment.java
@@ -1,11 +1,24 @@
 package com.example.doctorjava_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 /**
  * @author Created by Topias on 25/04/2019.
@@ -18,9 +31,49 @@ import androidx.fragment.app.Fragment;
  * in the screen view.
  */
 public class StatsFragment extends Fragment {
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    private static final String TAG = "StatsFragment";
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_stats, null);
     }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+
+        Button weekButton = getView().findViewById(R.id.buttonLastWeek);
+
+
+        final DayStatsDatabase db = Room.databaseBuilder(getContext(), DayStatsDatabase.class, "production")
+                .allowMainThreadQueries()
+                .build();
+
+        final List<DayStats> dayStatsList = db.dayStatsDao().getAllDayStats();
+
+        recyclerView = getView().findViewById(R.id.RecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        adapter = new DayStatsAdapter(dayStatsList);
+        recyclerView.setAdapter(adapter);
+
+        /*lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d(TAG, "onItemClick(" + i + ")");
+                Intent nextActivity = new Intent(MainActivity.this, DaysDetailsActivity.class);
+                nextActivity.putExtra(EXTRA, i);
+                startActivity(nextActivity);
+            }
+        });*/
+
+        weekButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: pressed!");
+                db.dayStatsDao().insertAll(new DayStats(130598, 3442234));
+            }
+        });
+
+    }
+
+
 }
