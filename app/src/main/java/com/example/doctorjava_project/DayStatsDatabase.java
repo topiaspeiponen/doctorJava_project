5a8fa0d19501444013b1d3622a887f239b35a14c
Topias <topias.peiponen@metropolia.fi>
2019-05-01 18:21:01 +0300

    Fixed dayId showing up in the adapter

--- app/src/main/java/com/example/doctorjava_project/DayStatsDatabase.java
+++ app/src/main/java/com/example/doctorjava_project/DayStatsDatabase.java
@@ -3,7 +3,7 @@ package com.example.doctorjava_project;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {DayStats.class}, version = 1)
+@Database(entities = {DayStats.class}, version = 1, exportSchema = false)
 public abstract class DayStatsDatabase extends RoomDatabase {
     public abstract DayStatsDao dayStatsDao();
 }
